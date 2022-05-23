2aeead4f73d31623562c3d40ffafbfe1dd4d40f0
ale.fomin <ale.fomin@tinkoff.ru>
2022-05-23 09:53:22 +0300

    feat: add try-catch at message messageConcatenation

--- src/main/java/com/tcs/edu/service/MessageConcatenator.java
+++ src/main/java/com/tcs/edu/service/MessageConcatenator.java
@@ -7,6 +7,11 @@ import java.util.stream.Stream;
 public class MessageConcatenator {
 
     public Message[] messageConcatenation(Message message, String... messages) {
-        return Stream.concat(Stream.of(message), Stream.of(messages)).toArray(Message[]::new);
+        try {
+            return Stream.concat(Stream.of(message), Stream.of(messages)).toArray(Message[]::new);
+        } catch (IllegalArgumentException e) {
+            throw new LogException("Wrong args!", e);
+        }
+        //return Stream.concat(Stream.of(message), Stream.of(messages)).toArray(Message[]::new);
     }
 }
