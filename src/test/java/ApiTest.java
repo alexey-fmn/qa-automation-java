1d4181855fff6e209c90b0f493369cb7f7e5fef8
ale.fomin <ale.fomin@tinkoff.ru>
2022-06-12 23:39:59 +0300

    feat: add api tests

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.restassured.RestAssured;
 import io.restassured.authentication.PreemptiveBasicAuthScheme;
@@ -12,9 +13,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-
-public class LocationTest {
+public class ApiTest {
 
     @BeforeAll
     public static void setUpAuth() {
@@ -35,7 +34,7 @@ public class LocationTest {
         given()
             .contentType("application/json")
             .body("{\n" +
-                "  \"countryName\": \"00\"\n" +
+                "  \"countryName\": \"03\"\n" +
                 "}")
             .when()
             .post("/api/countries")
@@ -61,7 +60,7 @@ public class LocationTest {
     // update
     @Test
     public void shouldUpdateEmployeeWhenGiven() {
-        Response response = given()
+        given()
             .contentType("application/json")
             .body("{\n" +
                 "  \"id\": 2 ,\n" +
@@ -70,16 +69,51 @@ public class LocationTest {
             .when()
             .patch("/api/employees/2")
             .then()
-            //.statusCode(200)
+            .statusCode(200)
             .body("lastName", equalTo("Grizzly"));
-
-        Assertions.assertEquals(200, response.statusCode());
 
     }
 
+    @Test
+    public void shouldCreateUserWhenGivenUnique() {
+        String requestBody = "{\n"
+            + "  \"login\": \"madmax335\",\n"
+            + "\"email\": \"asd2@qwe.com\"\n"
+            + "}";
+
+        given()
+            .contentType("application/json")
+            .body(requestBody)
+            .when()
+            .post("/api/admin/users")
+            .then()
+            .statusCode(201)
+            .body("id", not(empty()));
+
+    }
+
+
     // delete
     @Test
-    public void shouldDeleteUserRecordwhenGiven() {
+    public void shouldDeleteUserRecordWhenGiven() {
+        Response response = given()
+            .contentType("application/json")
+            .when()
+            .delete("/api/admin/users/madmax335")
+            .then()
+            .extract()
+            .response();
 
+        assertEquals(204, response.statusCode());
+    }
+
+    @Test
+    public void shouldReturnErrorWhenGetDeletedUser() {
+        Response response = when()
+            .get("/api/admin/users/madmax335")
+            .then()
+            .extract().response();
+
+        Assertions.assertEquals(404, response.statusCode());
     }
 }
